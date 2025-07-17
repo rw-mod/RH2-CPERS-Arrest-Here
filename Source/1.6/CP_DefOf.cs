a2d86106bbcafa5585363db81e8efd08c534ca37
Bounty <40918958+b0unt9@users.noreply.github.com>
2025-07-17 11:05:02 +0900

    Copy

---
+++ Source/1.6/CP_DefOf.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace CapturedPersons
+{
+    [DefOf]
+    public static class CP_DefOf
+    {
+        public static JobDef CP_ImprisonInPlace;
+        public static SoundDef CP_ArrestSound;
+    }
+}
